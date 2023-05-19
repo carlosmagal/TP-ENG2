db1cf604dce9e2db398b54624ff76c1f83c679ab
Luis Caetano <luisgabrielcaetano@gmail.com>
2023-05-19 17:02:53 -0300

    feat: adding new modules to the root app module so they're acessible at build time

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,8 +2,18 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { DietModule } from './diet/diet.module';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, UserModule, DietModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    AuthModule,
+    UserModule,
+    DietModule,
+    PrismaModule,
+  ],
 })
 export class AppModule {}
