cbf31301e6e33a71f237fa841682386443cf75c8
Luis Caetano <luisgabrielcaetano@gmail.com>
2023-05-19 16:59:56 -0300

    chore: adding necessary support code for swagger in main file

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,26 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+  });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('API Porta Soluções')
+    .setVersion('1.0')
+    .addTag('porta')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3333);
 }
 bootstrap();
