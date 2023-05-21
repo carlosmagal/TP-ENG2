e2cc1013f2bf157d9622e0327e2d477f426149f5
Renato Santos <renatossnt@gmail.com>
2023-05-21 20:15:15 -0300

    refactor: Extract getUserDataById from manualJwtExtraction Extract extractTokenFromAuthHeader from manualJwtExtraction

--- backend/src/auth/guard/jwt.guard.ts
+++ backend/src/auth/guard/jwt.guard.ts
@@ -1,8 +1,6 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
-import { AuthGuard } from '@nestjs/passport';
-import { Observable } from 'rxjs';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 async function manualJwtExtraction(
@@ -10,11 +8,10 @@ async function manualJwtExtraction(
   config: ConfigService,
   jwt: JwtService,
   prisma: PrismaService,
-) {
+): Promise<boolean> {
   const req = context.switchToHttp().getRequest();
   const authHeader: string = req.headers.authorization;
-  const splitHeader = authHeader.split('Bearer ');
-  const token = splitHeader[splitHeader.length - 1];
+  const token = extractTokenFromAuthHeader(authHeader);
   const jwtSecret = config.get('JWT_SECRET');
   try {
     const payload: {
@@ -23,16 +20,33 @@ async function manualJwtExtraction(
     } = jwt.verify(token, {
       secret: jwtSecret,
     });
-    const userData = await prisma.user.findUnique({
-      where: {
-        id: payload.sub,
-      },
-    });
+    const userData = await getUserDataById(payload.sub, prisma);
+
+    if (!userData) return false;
     req.user = userData;
     return true;
-  } catch (e) {
+  } catch (error) {
     return false;
   }
+}
+
+function extractTokenFromAuthHeader(authHeader: string): string | null {
+  const bearerPrefix = 'Bearer ';
+
+  if (authHeader && authHeader.startsWith(bearerPrefix)) {
+    return authHeader.substring(bearerPrefix.length);
+  }
+
+  return null;
+}
+
+
+async function getUserDataById(userId: string, prisma: PrismaService) {
+  return prisma.user.findUnique({
+    where: {
+      id: userId,
+    },
+  });
 }
 
 @Injectable()
