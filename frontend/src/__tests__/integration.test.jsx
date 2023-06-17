b7cd99f40a8c38a7772d5d90e4654415cb5ca2e4
Carlos Magalhães Silva <carlosmsilva755@gmail.com>
2023-06-17 15:04:16 -0300

    setup para testes no front

---
+++ frontend/src/__tests__/integration.test.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+// import { render, screen } from "@testing-library/react";
+
+describe("first", () => {
+  it("should render the component im testing", () => {
+    // render(<div />);
+    expect(1).toBe(1);
+  });
+});
